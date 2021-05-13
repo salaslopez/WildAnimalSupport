2d7ac0e2d8213d673568badb92febcbb20dd35de
salaslopez <salaszaray@gmail.com>
2021-05-13 19:21:27 +0000

    Workbeanch-working

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
   // DB Params
-  define("DB_HOST", "localhost");
-  define("DB_USER", "root");
-  define("DB_PASS", "pass");
+  define("DB_HOST", "wildanimalsupport-db.czmrv3j3e4zy.eu-west-1.rds.amazonaws.com");
+  define("DB_USER", "admin");
+  define("DB_PASS", "WildAnimalSupport2021");
   define("DB_NAME", "WildAnimalSupport");
   ?>
